0318159402c60a294b19776ba0f26268dff40093
Stanley Makazhe <devt3000@gmail.com>
2018-10-17 12:32:07 +0200

    import dependecies

---
+++ qa-api/src/index.js
@@ -0,0 +1,7 @@
+//import dependecies
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
