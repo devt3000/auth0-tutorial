ffe1b8e805300cb6718799bd38378e21ecc5bfcd
Stanley Makazhe <devt3000@gmail.com>
2018-10-17 21:32:06 +0200

    Making POST endpoints use the checkJwt middleware

--- qa-api/src/index.js
+++ qa-api/src/index.js
@@ -60,20 +60,21 @@ const checkJwt = jwt({
 });
 
 // insert a new question
-app.post("/", (req, res) => {
+app.post("/", checkJwt, (req, res) => {
   const { title, description } = req.body;
   const newQuestion = {
     id: questions.length + 1,
-    title: title,
-    description: description,
-    answers: []
+    title,
+    description,
+    answers: [],
+    author: req.user.name
   };
   questions.push(newQuestion);
   res.status(200).send();
 });
 
 // insert a new answer to a question
-app.post("/answer/:id", (req, res) => {
+app.post("/answer/:id", checkJwt, (req, res) => {
   const { answer } = req.body;
 
   const question = questions.filter(q => q.id === parseInt(req.params.id));
@@ -81,7 +82,8 @@ app.post("/answer/:id", (req, res) => {
   if (question.length === 0) return res.send(404).send();
 
   question[0].answers.push({
-    answer
+    answer,
+    author: req.user.name
   });
 
   res.status(200).send();
