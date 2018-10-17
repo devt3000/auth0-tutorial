0af52c93f7311f1d7d44f511f9423ee8f881c2ff
Stanley Makazhe <devt3000@gmail.com>
2018-10-17 12:53:58 +0200

    Insert a new answer to a question

--- qa-api/src/index.js
+++ qa-api/src/index.js
@@ -55,3 +55,18 @@ app.post("/", (req, res) => {
   questions.push(newQuestion);
   res.status(200).send();
 });
+
+// insert a new answer to a question
+app.post("/answer/:id", (req, res) => {
+  const { answer } = req.body;
+
+  const question = questions.filter(q => q.id === parseInt(req.params.id));
+  if (question.length > 1) return res.status(500).send();
+  if (question.length === 0) return res.send(404).send();
+
+  question[0].answers.push({
+    answer
+  });
+
+  res.status(200).send();
+});
